14507d860897bd7c76d61e05c1afef2e326715e6
Shankar1M <shankarmurthy9997@gmail.com>
2026-02-10 20:14:42 -0800

    complete arithmetic operation

--- src/intro/Arithmetic.java
+++ src/intro/Arithmetic.java
@@ -8,14 +8,35 @@ public class Arithmetic {
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		System.out.println("Choose below option to perform specific operation");
-		System.out.println("Option1 is Additon");
+		System.out.println("Option 1 is Additon");
 		System.out.println("Option 2 is Subtraction");
 		System.out.println("Option 3 is division");
 		System.out.println("Option 4 is multiplication");
 		int option = scan.nextInt();
+		if(option == 1)
+		{
+			int c = a+b;
+			System.out.println("Addditon of entered numbers "+c);
+		}
+		else if(option == 2)
+		{
+			int c = a-b;
+			System.out.println("Subtraction of entered numbers "+c);
+		}
 		
+		else if(option == 3)
+		{
+			int c = a/b;
+			System.out.println("division of entered numbers "+c);
+		}
+		else
+		{
+			int c = a*b;
+			System.out.println("multiplication of entered numbers "+c);
+
+		}
 		
-		
+	
 	}
 	
 
