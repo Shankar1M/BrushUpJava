53b72b5b2926b17762a7f8eb11b70a86164c3329
Shankar1M <shankarmurthy9997@gmail.com>
2026-02-10 20:03:49 -0800

    Operations started

--- src/intro/Arithmetic.java
+++ src/intro/Arithmetic.java
@@ -7,7 +7,12 @@ public class Arithmetic {
 		System.out.println("enter two number one beside the other: ");
 		int a = scan.nextInt();
 		int b = scan.nextInt();
-
+		System.out.println("Choose below option to perform specific operation");
+		System.out.println("Option1 is Additon");
+		System.out.println("Option 2 is Subtraction");
+		System.out.println("Option 3 is division");
+		System.out.println("Option 4 is multiplication");
+		int option = scan.nextInt();
 		
 		
 		
