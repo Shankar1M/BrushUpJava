1eb71c06045c78d8c91e730b1a7c7424ea58949b
Shankar1M <shankarmurthy9997@gmail.com>
2026-02-10 20:02:41 -0800

    Arithmetic operations

---
+++ src/intro/Arithmetic.java
@@ -0,0 +1,17 @@
+package intro;
+
+import java.util.*;
+public class Arithmetic {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter two number one beside the other: ");
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+
+		
+		
+		
+	}
+	
+
+}
