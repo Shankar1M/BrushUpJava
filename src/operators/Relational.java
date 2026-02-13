cdd4a16e73587ab9212f209085a8a80c3bee774f
Shankar1M <shankarmurthy9997@gmail.com>
2026-02-13 00:28:43 -0800

    Relational Operator starts

---
+++ src/operators/Relational.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class Relational {
+	public static void main(String[] args) {
+		// var is a keyword used to store dyanamic variables, var keyword automatically set the data depended on the value we gave.
+		var a = 10;
+		var b = 2;
+	
+		System.out.println("a == b "+(a==b));
+		System.out.println("a > b "+(a>b));
+		System.out.println("a >= b "+(a>=b));
+		System.out.println("a >= 10 "+(a>=10));
+		System.out.println("a <= b "+(a<=b));
+		System.out.println("a <= 10 "+(a<=10));
+		System.out.println("a != b "+(a!=b));
+		System.out.println("a != 10 "+(a!=10));
+
+
+		
+	}
+
+}
