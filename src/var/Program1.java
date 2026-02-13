531994a07a6043f183b26d1b2a7b4679d427aaeb
Shankar1M <shankarmurthy9997@gmail.com>
2026-02-12 22:50:07 -0800

    introduction to variable

---
+++ src/var/Program1.java
@@ -0,0 +1,13 @@
+package var;
+
+public class Program1 {
+	public static void main(String[] args) {
+		// var is a keyword used to store dyanamic variables, var keyword automatically set the data depended on the value we gave.
+		var a = 10;
+		var b = 20;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
