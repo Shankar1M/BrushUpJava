b67dda8d33b13e78ed5ba9a4dab0c8c2108e6faf
Shankar1M <shankarmurthy9997@gmail.com>
2026-02-13 00:07:02 -0800

    Operators section starts

---
+++ src/operators/Program1.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class Program1 {
+	public static void main(String[] args) {
+		// var is a keyword used to store dyanamic variables, var keyword automatically set the data depended on the value we gave.
+		var a = 10;
+		var b = 2;
+	/*	var c = a+b;
+		var d = a-b;
+		var e = a*b;
+		var f = a/b;
+		var g = a%b;  */
+
+		System.out.println("Addition result is "+(a+b));
+		System.out.println("Subtraction result is "+(a-b));
+		System.out.println("Multiplication result is "+(a*b));
+		System.out.println("Quotient result is "+(a/b));
+		System.out.println("Remainder result is "+(a%b));
+		
+	}
+
+}
