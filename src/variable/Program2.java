a06131a2db3384e579cfffa6be062538aaf89030
Shankar1M <shankarmurthy9997@gmail.com>
2026-02-12 23:12:25 -0800

    Different ways of using variable

---
+++ src/variable/Program2.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Program2 {
+	public static void main(String[] args) {
+		// var is a keyword used to store dyanamic variables, var keyword automatically set the data depended on the value we gave.
+		var a = "Ramesh";
+		var b = 40;
+		System.out.println("My name is "+a);
+		System.out.println("My age is "+b);
+		
+	}
+
+}
---
+++ src/variable/Program3.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Program3 {
+	public static void main(String[] args) {
+		// var is a keyword used to store dyanamic variables, var keyword automatically set the data depended on the value we gave.
+		var a = 10;
+		var b = 20;
+		System.out.println(a + b);
+		
+	}
+
+}
